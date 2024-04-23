dd824d6cb13508d26e57cde7e89026640c32dce5
TriNguyen1233 <167850127+TriNguyen1233@users.noreply.github.com>
2024-04-23 16:15:16 +0700

    tri
    add r=tanna

---
+++ Tenant.java
@@ -0,0 +1,36 @@
+
+package Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Asus
+ */
+public class Tenant {
+	private User user;
+	private List<RentalContract> listOfContracts;
+
+	public Tenant(User user) {
+		this.user = user;
+		this.listOfContracts = new ArrayList<>();
+	}
+
+	public void requestCreateRentalContract(RentalContract contract) {
+		listOfContracts.add(contract);
+	}
+
+	public void requestTerminateRentalContract(RentalContract contract) {
+		listOfContracts.remove(contract);
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public List<RentalContract> getListOfContracts() {
+		return listOfContracts;
+	}
+
+}
